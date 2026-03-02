a35af803c64cd4e2fd4fd586505fd25215850f59
ArturPenetra <arturpenetra@gmail.com>
2026-03-02 13:28:09 -0300

    First commit

---
+++ Marathon/Ex01.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.Marathon;
+
+class Ex01 {
+    public static void main(String[] args) {
+        //code
+    }
+}
---
+++ Marathon/Ex05.java
@@ -0,0 +1,4 @@
+package br.edu.ifsp.Marathon;
+
+public class Ex05 {
+}
